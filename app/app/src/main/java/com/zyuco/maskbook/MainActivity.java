9a1fca13c659c76294911e5d1989ad5667182f02
Juliiii <zhulf6@mail2.sysu.edu.cn>
2018-01-04 01:40:57 +0800

    太难了

--- app/app/src/main/java/com/zyuco/maskbook/MainActivity.java
+++ app/app/src/main/java/com/zyuco/maskbook/MainActivity.java
@@ -1,13 +1,32 @@
 package com.zyuco.maskbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    com.gc.materialdesign.views.ButtonFlat buttonSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListener();
     }
+
+
+    private void initListener() {
+        buttonSignup = findViewById(R.id.signup);
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+    }
+
 }
