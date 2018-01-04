9624def27f7dd94f65c6fd5c9fd3c26f926e8bab
Juliiii <zhulf6@mail2.sysu.edu.cn>
2018-01-05 03:38:31 +0800

    image-view

--- app/app/src/main/java/com/zyuco/maskbook/DashboardActivity.java
+++ app/app/src/main/java/com/zyuco/maskbook/DashboardActivity.java
@@ -2,12 +2,63 @@ package com.zyuco.maskbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.zyuco.maskbook.lib.CommonAdapter;
+import com.zyuco.maskbook.lib.ViewHolder;
+import com.zyuco.maskbook.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    CommonAdapter<Post> adapter;
+    List<Post> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        list.add(new Post());
+        list.add(new Post());
+        initList();
+    }
+
+    private void initList() {
+
+        adapter = new CommonAdapter<Post>(this, R.layout.post_item, list) {
+            @Override
+            public void convert(ViewHolder holder, Post data) {
+//                TextView name = holder.getView(R.id.name);
+//                name.setText(data.name);
+//                TextView belong = holder.getView(R.id.belong);
+//                belong.setText(data.belong);
+//                TextView description = holder.getView(R.id.abstract_description);
+//                description.setText("\t\t\t\t" + data.abstractDescription);
+                TextView name = holder.getView(R.id.name);
+                name.setText("123");
+                TextView content = holder.getView(R.id.content);
+                content.setText("咔咔");
+
+
+            }
+        };
+
+        adapter.setOnItemClickListemer(new CommonAdapter.OnItemClickListener<Post>() {
+            @Override
+            public void onClick(int position, Post data) {
+            }
+
+            @Override
+            public void onLongClick(int position, Post data) {
+            }
+        });
+
+
+        RecyclerView list = findViewById(R.id.post_list);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
     }
 }
