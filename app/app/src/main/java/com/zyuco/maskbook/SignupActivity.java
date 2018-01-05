4585d86d0e776558c9c666983640c80dd0b2f994
Juli <820917549@qq.com>
2018-01-06 01:34:23 +0800

    添加imageselector (#14)

--- app/app/src/main/java/com/zyuco/maskbook/SignupActivity.java
+++ app/app/src/main/java/com/zyuco/maskbook/SignupActivity.java
@@ -3,12 +3,19 @@ package com.zyuco.maskbook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AtomicFile;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
 import com.zyuco.maskbook.model.ErrorResponse;
 import com.zyuco.maskbook.model.User;
 import com.zyuco.maskbook.service.API;
@@ -17,6 +24,7 @@ import com.zyuco.maskbook.tool.CallBack;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import io.reactivex.functions.Action;
 
@@ -48,6 +56,13 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onRegistButtonClicked();
+            }
+        });
+
+        findViewById(R.id.avatar_wrapper).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
             }
         });
     }
@@ -128,4 +143,63 @@ public class SignupActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    private void chooseImage() {
+        PictureSelector
+                .create(SignupActivity.this)
+                .openGallery(PictureMimeType.ofImage())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                .maxSelectNum(1)// 最大图片选择数量 int
+                .imageSpanCount(4)// 每行显示个数 int
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
+                .previewImage(true)// 是否可预览图片 true or false
+                .isCamera(true)// 是否显示拍照按钮 true or false
+                .imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
+                .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                .enableCrop(true)// 是否裁剪 true or false
+                .compress(true)// 是否压缩 true or false
+                .freeStyleCropEnabled(true)// 裁剪框是否可拖拽 true or false
+                .circleDimmedLayer(false)// 是否圆形裁剪 true or false
+                .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false   true or false
+                .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false    true or false
+                .openClickSound(true)// 是否开启点击声音 true or false
+                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                .rotateEnabled(true) // 裁剪是否可旋转图片 true or false
+                .scaleEnabled(true)// 裁剪是否可放大缩小图片 true or false
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PictureConfig.CHOOSE_REQUEST:
+                    // 图片选择结果回调
+                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    // 例如 LocalMedia 里面返回三种path
+                    // 1.media.getPath(); 为原图path
+                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
+                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
+                    // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
+
+                    // 选取头像的路径
+                    String path;
+                    if (!selectList.isEmpty()) {
+                        LocalMedia localMedia = selectList.get(0);
+                        if (localMedia.isCut()) {
+                            if (localMedia.isCompressed()) {
+                                path = localMedia.getCompressPath();
+                            } else {
+                                path = localMedia.getCutPath();
+                            }
+                        } else {
+                            path = localMedia.getPath();
+                        }
+                        Glide.with(SignupActivity.this).load(new File(path)).into((ImageView) findViewById(R.id.avatar));
+                    }
+                    break;
+            }
+        }
+    }
+
 }
