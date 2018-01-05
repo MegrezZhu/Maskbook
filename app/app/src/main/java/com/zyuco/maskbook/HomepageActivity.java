d9ee7990fc1c83b4b73aa640305e8992f4019415
Juli <820917549@qq.com>
2018-01-05 13:31:49 +0800

    Feature/drawer (#10)
    * drawer
    
    * image-view
    
    * 主页UI雏形
    
    * homepage ui
    
    * purchase history ui
    
    * purchase history ui

--- app/app/src/main/java/com/zyuco/maskbook/HomepageActivity.java
+++ app/app/src/main/java/com/zyuco/maskbook/HomepageActivity.java
@@ -2,12 +2,57 @@ package com.zyuco.maskbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.zyuco.maskbook.lib.CommonAdapter;
+import com.zyuco.maskbook.lib.ViewHolder;
+import com.zyuco.maskbook.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomepageActivity extends AppCompatActivity {
+    CommonAdapter<Post> adapter;
+    List<Post> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        initList();
+    }
+
+    private void initList() {
+        list.add(new Post());
+        list.add(new Post());
+        adapter = new CommonAdapter<Post>(this, R.layout.post_item, list) {
+            @Override
+            public void convert(ViewHolder holder, Post data) {
+                TextView name = holder.getView(R.id.name);
+                name.setText("123");
+                TextView content = holder.getView(R.id.content);
+                content.setText("咔咔");
+            }
+        };
+
+        adapter.setOnItemClickListemer(new CommonAdapter.OnItemClickListener<Post>() {
+            @Override
+            public void onClick(int position, Post data) {
+            }
+
+            @Override
+            public void onLongClick(int position, Post data) {
+            }
+        });
+
+
+        RecyclerView list = findViewById(R.id.post_list);
+
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setNestedScrollingEnabled(false);
+        list.setAdapter(adapter);
     }
 }
